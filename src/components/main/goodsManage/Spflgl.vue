0a5de1a05c4e38021fc91708106066c880457a8d
isoma <somanei@qq.com>
2020-04-28 11:50:44 +0800

    -- 加注释 -- 修改回显有问题版本 -- 回显ok 更改数据源完成

--- src/components/main/goodsManage/Spflgl.vue
+++ src/components/main/goodsManage/Spflgl.vue
@@ -1,196 +1,135 @@
 <template>
-    <div class="spflgl">
-      <h1>商品分类管理</h1>
-      <el-breadcrumb separator="/" style="height: 30px;margin: 15px 10px;">
-        <el-breadcrumb-item >首页</el-breadcrumb-item>
-        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-      </el-breadcrumb>
-      <el-button-group>
-        <el-button @click="dialogVisible = true">新建</el-button>
-        <el-button @click="delBrand">删除</el-button>
-        <el-button >刷新</el-button>
-      </el-button-group>
+  <div class="spflgl">
+    <h1>商品分类管理</h1>
+    <el-breadcrumb separator-class="el-icon-arrow-right"  style="height: 30px;margin: 15px 10px;font-size: 20px;">
+      <el-breadcrumb-item >
+        <span v-if="tbItemCatId === 0 || tbItemCatId === ''" @click="showOne()" style="font-weight: 900">顶级分类列表</span>
+        <span v-else @click="showOne()">顶级分类列表</span>
+      </el-breadcrumb-item>
+      <el-breadcrumb-item >
 
-      <el-table :data="tableData" border @selection-change="handleSelectionChange" style="width: 100%">
-        <el-table-column prop="brandId" type="selection" width="180">
-        </el-table-column>
-        <el-table-column prop="brandId" label="商家ID" width="180">
-        </el-table-column>
-        <el-table-column prop="brandName" label="品牌名称">
-        </el-table-column>
-        <el-table-column prop="brandFirst" label="品牌首字母">
-        </el-table-column>
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-button-group>
+      <el-button @click="dialogVisible = true">新建</el-button>
+      <el-button>删除</el-button>
+      <el-button>刷新</el-button>
+    </el-button-group>
+
+    <el-table :data="tableData" border @selection-change="handleSelectionChange" style="width: 100%">
+      <el-table-column prop="id" type="selection" width="180">
+      </el-table-column>
+      <el-table-column prop="id" label="分类ID" width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="分类名称">
+      </el-table-column>
+      <el-table-column prop="typeId" label="类型模板ID">
+      </el-table-column>
 
 
-        <el-table-column prop="address" label="操作">
-          <template slot-scope="scope">
-            <el-button class="b" size="mini" @click="handleEdit(scope.$index, scope.row),dialogVisibles=true">修改</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <el-table-column prop="address" label="操作">
+        <template slot-scope="scope">
+          <el-button size="mini" type="primary" @click="getNext(scope.row)">获取下级</el-button>
+          <el-button class="b" size="mini" type="warning" @click="handleEdit(scope.$index, scope.row),dialogVisibles=true">修改
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
-      <template>
-        <div style="text-align: center;margin-top: 20px;">
-          <el-pagination
-            @current-change="handleCurrentChange"
-            :current-page="currentPage"
-            :page-size="pageSize" layout="total, prev, pager, next, jumper"
-            :total="totalCount">
-          </el-pagination>
-        </div>
-      </template>
+    <template>
+      <div style="text-align: center;margin-top: 20px;">
+        <el-pagination
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-size="5" layout="total, prev, pager, next, jumper"
+          :total="totalCount">
+        </el-pagination>
+      </div>
+    </template>
 
-      <el-dialog title="新增" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-        品牌:<el-input placeholder="请输入品牌" v-model="inputPp" @blur="getFirst(inputPp)" clearable></el-input>
-        首字母:<el-input disabled placeholder="请输入首字母" v-model="inputSzm" ></el-input>
-        <span slot="footer" class="dialog-footer">
+    <el-dialog title="新增" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+      品牌:
+      <el-input placeholder="请输入品牌" clearable></el-input>
+      首字母:
+      <el-input disabled placeholder="请输入首字母"></el-input>
+      <span slot="footer" class="dialog-footer">
           <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="addBrand(),clear()">确 定</el-button>
+          <el-button type="primary">确 定</el-button>
         </span>
-      </el-dialog>
+    </el-dialog>
 
-      <el-dialog title="修改" :visible.sync="dialogVisibles" width="30%" :before-close="handleClose">
-        品牌:<el-input placeholder="请输入品牌" v-model="pp" @blur="getFirsts(pp)" clearable></el-input>
-        首字母:<el-input disabled placeholder="请输入首字母" v-model="szm" ></el-input>
-        <span slot="footer" class="dialog-footer">
-          <el-button @click="dialogVisibles = false">取 消</el-button>
-          <el-button type="primary" @click="updBrand()">确 定</el-button>
-        </span>
-      </el-dialog>
-    </div>
+
+  </div>
 </template>
 
 <script>
-    import {Pinyin} from "../../../../static/ChinesePY";
-
-    export default {
-      name: "Spflgl",
-      data(){
-        return{
-          tableData:[],
-          dialogVisible: false,
-          inputPp:"",
-          inputSzm:"",
-          currentPage: 1,
-          // 总条数，根据接口获取数据长度(注意：这里不能为空)
-          totalCount: 1,
-          // 默认每页显示的条数（可修改）
-          pageSize: 5,
-          multipleSelection: [],
-          pp:"",
-          szm:"",
-          dialogVisibles:false,
-          py:Pinyin,
-          brandId:""
-        }
+  export default {
+    name: "Spflgl",
+    data() {
+      return {
+        tableData: [],
+        dialogVisible: false,
+        currentPage: 1,
+        // 总条数，根据接口获取数据长度(注意：这里不能为空)
+        totalCount: 1,
+        // 默认每页显示的条数（可修改）
+        multipleSelection: [],
+        dialogVisibles: false,
+        tbItemCatId:""
+      }
+    },
+    created() {
+      this.$http.get('http://localhost:8082/tbItemCat/getLevel/0/0/5').then(res => {
+        this.tableData = res.data;
+        this.tbItemCatId = res.data[0].parentId;
+      });
+      this.$http.get('http://localhost:8082/tbItemCat/getCount/0').then(res => {
+        this.totalCount = res.data;
+      });
+    },
+    methods: {
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
       },
-      methods:{
-        handleSelectionChange(val) {
-          this.multipleSelection = val;
-        },
-        handleEdit(index,row){
-          this.pp = row.brandName;
-          this.szm = row.brandFirst;
-          this.brandId = row.brandId;
-        },
-        clear(){
-          this.inputPp="";
-          this.inputSzm = "";
-        },
-        handleClose(done) {
-          this.$confirm('确认关闭？')
-            .then(_ => {
-              done();
-            })
-            .catch(_ => {});
-        },
-        handleCurrentChange(val){
-          this.currentPage = val;
-          this.getData();
-        },
-        getData(){
-          this.$http.get('http://localhost:8082/brand/getAll',{params:{
-              pageSize: this.pageSize,
-              // 显示第几页
-              curPage: (this.currentPage - 1) * this.pageSize
-            }}).then(res=>{
-            this.tableData = res.data;
+      handleEdit(index, row) {
+      },
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {
           });
-          this.$http.get('http://localhost:8082/brand/getCount').then(res=>{
-            this.totalCount = res.data;
-          });
-        },
-        delBrand(){
-          if (this.multipleSelection.length == 0){
-            this.$message({message: '删除不得为空', type: 'warning'});
-          }else{
-            this.$http.post('http://localhost:8082/brand/delBrand', this.multipleSelection).then(res=>{
-              if (res.data == 1 ){
-                this.$message({message: '删除成功!', type: 'success'});
-                this.getData();
-              }else{
-                this.$message({message: '删除失败!', type: 'warning'});
-              }
-            })
-          }
-        },
-        getFirst(inputPp){
-          const szm = this.py.GetJP(inputPp);
-          this.inputSzm = szm.substring(0,1).toUpperCase();
-        },
-        getFirsts(pp){
-          const szm = this.py.GetJP(pp);
-          this.szm = szm.substring(0,1).toUpperCase();
-        },
-        addBrand(){
-          if (this.inputPp == ""){
-            this.$message({message: '不得为空!', type: 'warning'});
-            return;
-          }else{
-            this.$http.post('http://localhost:8082/brand/addBrand',{
-              brandName:this.inputPp,brandFirst:this.inputSzm
-            }).then(res=>{
-              console.log(res)
-              if (res.data == 1){
-                this.$message({message: '新增成功!', type: 'success'});
-                this.$http.get('http://localhost:8082/brand/getCount').then(res=>{
-                  this.totalCount = res.data;
-                });
-                this.clear();
-                this.dialogVisible = false;
-              }else{
-                this.$message({message: '新增失败!', type: 'warning'});
-              }
-            })
-          }
-        },
-        updBrand(){
-          if (this.pp == ""){
-            this.$message({message: '不得为空', type: 'warning'});
-          }else {
-            this.$http.post('http://localhost:8082/brand/updBrand', {
-              brandName: this.pp, brandFirst: this.szm,brandId:this.brandId
-            }).then(res => {
-              console.log(res);
-              if (res.data == 1) {
-                this.$message({message: '修改成功!', type: 'success'});
-                this.getData();
-              }else{
-                this.$message({message: '修改失败!', type: 'warning'});
-              }
-              this.clear();
-              this.dialogVisibles = false;
-            })
-          }
-        }
+      },
+      handleCurrentChange(val) {
+        this.currentPage = val;
+        this.tbItemCatId = this.tableData[0].parentId;
+        this.getData(this.tbItemCatId);
+      },
+      getNext(row) {
+        this.tbItemCatId = row.id;
+        this.getData(this.tbItemCatId);
+      },
+      getData(id){
+        this.$http.get("http://localhost:8082/tbItemCat/getLevel/"+id+"/"+((this.currentPage -1)*5)+"/5").then(res => {
+          this.tableData = res.data;
+        });
+        this.$http.get('http://localhost:8082/tbItemCat/getCount/'+id).then(res => {
+          this.totalCount = res.data;
+        });
+      },
+      showOne(){
+        this.getData(0);
+      },
+      showTwo(){
+
       }
     }
+  }
 </script>
 
 <style scoped>
-  .spflgl{
+  .spflgl {
     margin-left: 202px;
   }
 </style>
